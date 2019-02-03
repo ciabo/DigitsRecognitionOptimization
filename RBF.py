8513517a6e4359f329899510040456098dc732da
ettore <ettorecelozzi@gmail.com>
2019-02-03 16:02:49 +0100

    rbf plotting

--- RBF.py
+++ RBF.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+
 class RBF():
-    def __init__(self,X,F):
-        self.X=X
-        self.F=F
+    def __init__(self, X, F):
+        self.X = X
+        self.F = F
         # default epsilon is the "the average distance between nodes" based
         # on a bounding hypercube
         ximax = np.amax(self.X, axis=0)
@@ -11,31 +12,30 @@ class RBF():
         edges = ximax - ximin
         self.epsilon = np.power(np.prod(edges) / self.X.size, 1.0 / edges.size)
 
-
-    def gaussian(self,xi,yi):
-        r=np.sqrt(np.power( (xi-yi) , 2))#norm of xi-yi
+    def gaussian(self, xi, yi):
+        r = np.sqrt(np.power((xi - yi), 2))  # norm of xi-yi
         return np.exp(-(1.0 / self.epsilon * r) ** 2)
 
     def interpolate(self):
         n = self.X.size
-        self.phi = np.zeros((n,n),dtype=float)
-        for i in range(0,n):
-            for j in range(0,n):
-                v=self.gaussian(self.X[i],self.X[j])
+        self.phi = np.zeros((n, n), dtype=float)
+        for i in range(0, n):
+            for j in range(0, n):
+                v = self.gaussian(self.X[i], self.X[j])
                 self.phi[i][j] = v
-        self.multipliers = np.linalg.solve(self.phi,self.F)
+        self.multipliers = np.linalg.solve(self.phi, self.F)
 
     def getMultipliers(self):
         return self.multipliers
 
-    def newxGivenf(self,fvalue):
-        xcap=0 #value we want to minimize
+    def newxGivenf(self, fvalue):
+        xcap = 0  # value we want to minimize
+        np.power(fvalue - self.s(xcap), 2) * self.g(xcap)
 
-        np.power(fvalue-self.s(xcap) , 2)*self.g(xcap)
-    def g(self,xcap):
-        a=np.linalg.det(self.phi)
+    def g(self, xcap):
+        a = np.linalg.det(self.phi)
 
-    def s(self,xcap):
-        for i in range(0,self.X.size):
-            sxcap=self.multipliers[i]*self.gaussian(xcap,self.X[i])
+    def s(self, xcap):
+        for i in range(0, self.X.size):
+            sxcap = self.multipliers[i] * self.gaussian(xcap, self.X[i])
         return sxcap
